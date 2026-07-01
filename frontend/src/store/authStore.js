5fe5a253f3f3c093fc4e91ca05b4b0a4f5c45f12
Sarthak Baghla <Sarthak.Baghla@Invedus.com>
2026-07-01 19:20:01 +0530

    vercel v2

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API_URL = "/auth/api";
+const API_URL = "https://advanced-authentication-full-stack-4.onrender.com/auth/api";
 // const API_URL = "http://localhost:6060/auth/api";
 
 axios.defaults.withCredentials = true;
